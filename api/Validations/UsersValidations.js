56eb40a384a80c110fe0b9f8f30af48e79e25fe9
Shivani <shivani@jcsoftwaresolution.com>
2024-04-08 14:18:12 +0530

    change validation of tax classification key

--- api/Validations/UsersValidations.js
+++ api/Validations/UsersValidations.js
@@ -159,7 +159,7 @@ exports.addUser = async (req, res, next) => {
 
         federal_text_classification: Joi.string().optional().valid("individual", "s_corporation", "c_corporation", "patnership", "limited_liability",),
         social_security_number: Joi.string().optional(),
-        tax_classification: Joi.string().required(),
+        tax_classification: Joi.string().optional(),
         tax_name: Joi.string().required(),
         is_us_citizen: Joi.boolean().optional(),
         trade_name: Joi.string().optional().allow(""),
