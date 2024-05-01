34353cb6ac18ccdd333b9969674546e9623076a2
Shivani <shivani@jcsoftwaresolution.com>
2024-05-01 13:03:06 +0530

    remove shortUrl from schema of affiliate link

--- api/models/AffiliateLink.js
+++ api/models/AffiliateLink.js
@@ -11,7 +11,6 @@ module.exports = {
   attributes: {
     affiliate_id: { model: "users" },
     link: { type: "string" },
-    shortUrl: { type: 'string', unique: true, required: true },
     updatedBy: { model: "users" },
     createdAt: { type: "ref", autoCreatedAt: true, },
     updatedAt: { type: "ref", autoUpdatedAt: true, },
