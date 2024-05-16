5f6049b277b0591ebb9791ea6d9e0f38b26d0c5f
Shivani <shivani@jcsoftwaresolution.com>
2024-05-16 18:30:36 +0530

    validation in commission

---
+++ api/Validations/CommissionNewValidations.js
@@ -0,0 +1,41 @@
+const Joi = require('joi');
+const Validate = require('./Validate').validate;
+
+exports.addCommission = async (req, res, next) => {
+    const schema = Joi.object({
+        upload_method: Joi.string().optional().valid("single", "batch"),
+        commission_type: Joi.string().optional().valid("sales", "lead"),
+        publisher_id: Joi.number().optional(),
+        amount_of_sale: Joi.number().optional(),
+        amount_of_commission: Joi.number().optional(),
+        order_reference: Joi.string().optional(),
+        click_ref: Joi.string().optional(),
+        affiliate_id: Joi.string().required().allow(null),
+        is_send_email_to_publisher: Joi.boolean().optional()
+    });
+    return await Validate(schema, req, res);
+
+}
+exports.editCommission = async (req, res, next) => {
+
+    const schema = Joi.object({
+        id: Joi.string().required(),
+        title: Joi.string().optional(),
+        seo_attributes: Joi.string().optional(),
+        destination_url: Joi.string().optional(),
+        description: Joi.string().optional(),
+        image: Joi.string().optional(),
+        activation_date: Joi.date().optional().allow(""),
+        availability_date: Joi.date().optional().allow(""),
+        expiration_date: Joi.date().optional().allow(""),
+        is_animation: Joi.boolean().optional(),
+        is_deep_linking: Joi.boolean().optional(),
+        mobile_creative: Joi.boolean().optional(),
+        category_id: Joi.string().optional().allow(null)
+
+    });
+    return await Validate(schema, req, res);
+
+}
+
+
