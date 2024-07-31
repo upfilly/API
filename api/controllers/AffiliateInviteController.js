4b98e39dbce51a36d6d910ab8c588eab8d7b91d0
deepanshuchaudhary <deepanshuchaudhary@jcsoftwaresolution.in>
2024-07-31 18:43:53 +0530

    fix

--- api/controllers/AffiliateInviteController.js
+++ api/controllers/AffiliateInviteController.js
@@ -34,8 +34,11 @@ module.exports = {
         isDeleted: false,
       });
 
+      console.log(result,"================");
       if (result.length === 0) {
+
         for await (let affiliate_id of data.affiliate_id) {
+
           let result1 = await AffiliateInvite.create({
             affiliate_id: affiliate_id,
             message: data.message,
@@ -58,33 +61,6 @@ module.exports = {
               brand_name: brand_detail.fullName,
               affiliate_name: brand_detail.fullName,
             };
-
-            let notification_payload = {};
-            // notification_payload.send_to = add_campaign.affiliate_id;
-            notification_payload.title = `Brand Invite | ${await Services.Utils.title_case(
-              data.fullName
-            )} | ${await Services.Utils.title_case(req.identity.fullName)}`;
-            notification_payload.message = `You have a new Brand invite from ${await Services.Utils.title_case(
-              req.identity.fullName
-            )}`;
-            notification_payload.type = "brand_request";
-            notification_payload.addedBy = req.identity.id;
-            notification_payload.send_to = data.id;
-            let create_notification = await Notifications.create(
-              notification_payload
-            ).fetch();
-
-            let affiliate_detail = await Users.findOne({ id: data.id });
-            if (create_notification && affiliate_detail.device_token) {
-              let fcm_payload = {
-                device_token: affiliate_detail.device_token,
-                title: req.identity.fullName,
-                message: create_notification.message,
-              };
-
-              await Services.FCM.send_fcm_push_notification(fcm_payload);
-            }
-
             await Emails.OnboardingEmails.send_mail_to_affiliate(emailpayload);
             return response.success(
               null,
