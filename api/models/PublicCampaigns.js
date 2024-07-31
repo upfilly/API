a08db1e5627e7646633a249049a273d0b1c48f3e
deepanshuchaudhary <deepanshuchaudhary@jcsoftwaresolution.in>
2024-07-31 18:22:43 +0530

    feat:added some changes in campaign

---
+++ api/models/PublicCampaigns.js
@@ -0,0 +1,48 @@
+/**
+ * Publiccampaigns.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    affiliate_id:{
+      model: 'users'
+    },
+    campaign_id:{
+      model: 'campaign'
+    },
+    brand_id: {
+      model: 'users'
+    },
+    isDeleted: {
+      type: 'Boolean',
+      defaultsTo: false
+    },
+    deletedBy: {
+      model: 'users'
+    },
+    deletedAt: {
+      type: 'ref',
+      columnType: 'datetime'
+    },
+    updatedBy: {
+      model: 'users'
+    },
+    addedBy: {
+      model: 'Users'
+    },
+    createdAt: {
+      type: 'ref',
+      autoCreatedAt: true
+    },
+    updatedAt: {
+      type: 'ref',
+      autoUpdatedAt: true
+    }
+  },
+
+};
+
