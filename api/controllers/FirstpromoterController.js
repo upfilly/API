623252ce875d429112c0e6d401fbf5c24e2c93b8
deepanshuchaudhary <deepanshuchaudhary@jcsoftwaresolution.in>
2024-08-06 17:04:53 +0530

    fix

--- api/controllers/FirstpromoterController.js
+++ api/controllers/FirstpromoterController.js
@@ -148,7 +148,10 @@ module.exports = {
     const newFileName = `file_${uniqueId}.csv`; // Adjust file extension as needed
 
     // Launch the browser
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      headless: true,
+    });
     const page = await browser.newPage();
 
     // Set up download behavior
