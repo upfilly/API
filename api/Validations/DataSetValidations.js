29f32bc27e8259f8c119883cc78d2cb2ad350477
Ashish Mittal <ashishm@jcsoftwaresolution.com>
2025-01-20 15:18:50 +0530

    Fix send dataset code

--- api/Validations/DataSetValidations.js
+++ api/Validations/DataSetValidations.js
@@ -5,7 +5,7 @@ exports.addDataSet = async (req, res, next) => {
   const schema = Joi.object({
     // user_id:Joi.string().required(),
     filePath:Joi.string().required(),
-    brand_id: Joi.string().required()
+    // brand_id: Joi.string().required()
   });
   return await Validate(schema, req, res);
 };
