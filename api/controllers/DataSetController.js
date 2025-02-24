48bd29de6f0ce104681ba78a1f9bc7dd187c09fe
shiv <shiv@gmail.com>
2025-02-24 19:24:23 +0530

    adding consoles

--- api/controllers/DataSetController.js
+++ api/controllers/DataSetController.js
@@ -471,7 +471,7 @@ exports.sendDataSets = async (req, res) => {
         
         let csv_url = urlData
         console.log(csv_url,'=====')
-        csv_url = csv_url.split("/")[2]
+        csv_url = csv_url.split("/")[3] // on server on 3rd index url_docs is coming
         console.log(csv_url,'csv_url')
         var rootpath = process.cwd();
         const csvPath = rootpath + "/assets/url_docs/"+ csv_url //path.join(__dirname, 'data.csv'); // Path relative to script
