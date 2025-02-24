437980863dde91b361b05d85fbb18d91153c3c2a
shiv <shiv@gmail.com>
2025-02-24 19:28:17 +0530

    adding consoles

--- api/controllers/DataSetController.js
+++ api/controllers/DataSetController.js
@@ -471,7 +471,7 @@ exports.sendDataSets = async (req, res) => {
         
         let csv_url = urlData
         console.log(csv_url,'=====')
-        csv_url = csv_url.split("/")[4] // on server 
+        csv_url = csv_url.split("/") // on server 
         csv_url = csv_url.pop()
         console.log(csv_url,'csv_url')
         var rootpath = process.cwd();
