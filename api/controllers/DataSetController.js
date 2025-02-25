1513962dddcb0726da9429f970c2d53412fb8f6e
shiv <shiv@gmail.com>
2025-02-25 15:33:51 +0530

    region issue fixed

--- api/controllers/DataSetController.js
+++ api/controllers/DataSetController.js
@@ -644,9 +644,10 @@ exports.sendDataSets = async (req, res) => {
       let updatedCSV = await processCSVAndRespond(csvPath,newColumn,req.identity.id)
       console.log(updatedCSV,'updatedCSV')
       
-        const xmlFilePath = constant.BACK_WEB_URL + "/assets/documents/" + updatedCSV
+        const xmlFile = constant.BACK_WEB_URL + "/assets/documents/" + updatedCSV
+        const xmlFilePath = rootpath +  "/assets/documents/" + generateName() + ".xml"
         let id = req.identity.id
-        let xml = await fetchAndUpdateXML(url, xmlFilePath,id);
+        let xml = await fetchAndUpdateXML(xmlFile, xmlFilePath,id);
 
         xml = xml.split("/")
         xml = xml.splice(-2)
