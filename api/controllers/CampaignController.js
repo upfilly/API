b848745200e1cbb437a93b13929812da3828cc76
shiv <shiv@gmail.com>
2025-03-21 11:35:11 +0530

    id is not defined

--- api/controllers/CampaignController.js
+++ api/controllers/CampaignController.js
@@ -296,6 +296,7 @@ exports.getAllCampaignRequestsForAffiliate = async (req, res) => {
                 {source: "campaign", status: "accepted"},
                 {source: "campaign", status: "rejected"},
                 {source: "campaign", status: "pending"},
+                {source: "campaign", status: "requested"},
                 {source: "invite", status: "accepted"},
                 {source: "make_offer", status: "accepted"},
                 {source: "affiliate_request", status: "accepted"}
