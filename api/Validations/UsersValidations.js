6e4b84f9db6c6a171559617b97d89689ebe73025
shiv <shiv@gmail.com>
2025-05-12 15:30:03 +0530

    joi

--- api/Validations/UsersValidations.js
+++ api/Validations/UsersValidations.js
@@ -361,7 +361,7 @@ exports.editProfile = async (req, res, next) => {
         signature: Joi.string().optional().allow(""),
         timezone: Joi.string().optional().allow(""),
         signature_date: Joi.date().optional().allow(""),
-        currencies: Joi.array().optional().allow([]),
+        currencies: Joi.array().optional().allow(null),
     });
     return await Validate(schema, req, res);
 
