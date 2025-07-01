9b6fb9ec7c01e82b5575c976cc406ae7d70fd9d3
Rishav <rishavn@jcsoftwaresolution.com>
2025-07-01 14:49:56 +0530

    changes done

--- api/controllers/AffiliateLinkController.js
+++ api/controllers/AffiliateLinkController.js
@@ -386,8 +386,8 @@ exports.find = async function (req, res) {
           price: obj?.price,
           order_id: obj?.order_id,
           commission: obj?.commission ? obj?.commission_type === "amount" ? `$${obj?.commission}` : `${obj?.commission}%` : "--",
-          amount_of_commission: obj?.amount_of_commission,
-          // commission_paid: calculatetotalCommission(obj?.commission_type, obj?.price, obj?.commission, commission_override),
+          // amount_of_commission: obj?.amount_of_commission,
+           amount_of_commission: calculatetotalCommission(obj?.commission_type, obj?.price, obj?.commission, commission_override),
           commission_paid: obj?.commission_paid,
           commission_status: obj?.commission_status,
           counter: counter
