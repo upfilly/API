a87e89514aa5373a820ea9053feb4266385f880e
shiv <shiv@gmail.com>
2025-08-11 11:56:51 +0530

    add category type

--- api/Validations/CommonCategoryValidations.js
+++ api/Validations/CommonCategoryValidations.js
@@ -5,7 +5,7 @@ exports.addCommonCategory = async (req, res, next) => {
 
     const schema = Joi.object({
         type: Joi.string().required().valid("sub", "main"),
-        cat_type: Joi.string().optional().valid("faq", "blog", "product", "promotional_models", "property_types", "advertiser_categories"),
+        cat_type: Joi.string().optional().valid("faq", "blog", "product", "promotional_models", "property_types", "advertiser_categories","creative_assets"),
         parent_id: Joi.string().when('type', {
             is: "sub",
             then: Joi.string().required(),
--- api/models/CommonCategories.js
+++ api/models/CommonCategories.js
@@ -13,7 +13,7 @@ module.exports = {
         },
         cat_type: {
             type: 'string',
-            isIn: ["faq", "blog", "product","promotional_models","property_types","advertiser_categories"]
+            isIn: ["faq", "blog", "product","promotional_models","property_types","advertiser_categories","creative_assets"]
         },
         type: {
             type: 'string',
