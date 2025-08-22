5198fc024dce2dab14efb3d7855d5cf16e8ff744
rishav <rishavn@jcsoftwaresolution.com>
2025-08-22 13:41:20 +0530

    changes

--- config/constants.js
+++ config/constants.js
@@ -631,7 +631,8 @@ module.exports.constants = {
     FETCHED_ALL: "All Coupon Fetched successfully",
     ALREADY_EXISTS: "Coupon code already exists",
     NOT_EXISTS: "Coupon not exists",
-    CAMAPIGN : "Campaign id not exist."
+    CAMAPIGN : "Campaign id not exist.",
+    START_DATE_OVERLAPED : "Start Date over load."
   },
   CSVDATA: {
     IMPORTED_SUCCESSFULLY: "Data imported successfully",
