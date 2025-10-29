d90fa98d9a5f1a92d5440035c017854b92f3d885
Akshay <akshaysharma@jcsoftwaresolution.com>
2025-10-29 11:30:22 +0530

    add changes in first promoter

--- api/controllers/FirstpromoterController.js
+++ api/controllers/FirstpromoterController.js
@@ -366,114 +366,12 @@ exports.getAllFirstPromoters = async (req, res) => {
     let { search, isDeleted, status, sortBy, addedBy } = req.query;
     let skipNo = (Number(page) - 1) * Number(count);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-        if (search) {
-            search = Services.Utils.remove_special_char_exept_underscores(search);
-            query.$or = [
-                { email: { $regex: search, '$options': 'i' } },
-                { url: { $regex: search, '$options': 'i' } },
-            ];
-        }
-
-        if (isDeleted) {
-            query.isDeleted = isDeleted === 'true';
-        } else {
-            query.isDeleted = false;
-        }
-
-        if (status) {
-            query.status = status;
-        }
-
-         let sortquery = {};
-         if (sortBy && typeof sortBy === "string") {
-           const [rawField, rawOrder] = sortBy.trim().split(/\s+/);
-           const field = rawField || "createdAt";
-           const sortType = rawOrder?.toLowerCase() === "asc" ? 1 : -1;
-           sortquery[field] = sortType;
-         } else {
-           sortquery = { updatedAt: -1 };
-         }
-
-        if (addedBy) {
-            query.addedBy = new ObjectId(addedBy);
-        }
-
-        // Pipeline Stages
-        let pipeline = [
-            {
-                $lookup: {
-                    from: 'users',
-                    localField: 'addedBy',
-                    foreignField: '_id',
-                    as: "addedBy_details"
-                }
-            },
-            {
-                $unwind: {
-                    path: '$addedBy_details',
-                    preserveNullAndEmptyArrays: true
-                }
-            },
-        ];
-
-        let projection = {
-            $project: {
-                id: "$_id",
-                email: "$email",
-                url: "$url",
-                status: "$status",
-                addedBy: "$addedBy",
-                addedBy_name: "$addedBy_details.fullName",
-                updatedBy: "$updatedBy",
-                updatedAt: "$updatedAt",
-                isDeleted: "$isDeleted",
-                createdAt: "$createdAt",
-                updatedAt: "$updatedAt",
-            }
-        };
-
-        pipeline.push(projection);
-        pipeline.push({
-            $match: query
-        });
-        pipeline.push({
-            $sort: sortquery
-        });
-
-        let totalresult = await db.collection('firstpromoter').aggregate(pipeline).toArray();
-        pipeline.push({
-            $skip: Number(skipNo)
-        });
-        pipeline.push({
-            $limit: Number(count)
-        });
-        let result = await db.collection("firstpromoter").aggregate(pipeline).toArray();
-        let resData = {
-            total_count: totalresult ? totalresult.length : 0,
-            data: result ? result : [],
-        };
-        if (!req.param('page') && !req.param('count')) {
-            resData = totalresult ? totalresult : [];
-        }
-        return response.success(resData, constants.FIRST_PROMOTER.FETCHED_ALL, req, res);
-
-    } catch (error) {
-        return response.failed(null, `${error}`, req, res);
-=======
-=======
->>>>>>> 8cadf2970c861c4dfbde9064bc29a37992964f2c
     if (search) {
       search = Services.Utils.remove_special_char_exept_underscores(search);
       query.$or = [
         { email: { $regex: search, '$options': 'i' } },
         { url: { $regex: search, '$options': 'i' } },
       ];
-<<<<<<< HEAD
->>>>>>> 8cadf2970c861c4dfbde9064bc29a37992964f2c
-=======
->>>>>>> 8cadf2970c861c4dfbde9064bc29a37992964f2c
     }
 
     if (isDeleted) {
@@ -487,11 +385,11 @@ exports.getAllFirstPromoters = async (req, res) => {
     }
 
     let sortquery = {};
-    if (sortBy) {
-      let typeArr = sortBy.split(" ");
-      let sortType = typeArr[1];
-      let field = typeArr[0];
-      sortquery[field ? field : 'createdAt'] = sortType ? (sortType === 'desc' ? -1 : 1) : -1;
+    if (sortBy && typeof sortBy === "string") {
+      const [rawField, rawOrder] = sortBy.trim().split(/\s+/);
+      const field = rawField || "createdAt";
+      const sortType = rawOrder?.toLowerCase() === "asc" ? 1 : -1;
+      sortquery[field] = sortType;
     } else {
       sortquery = { updatedAt: -1 };
     }
