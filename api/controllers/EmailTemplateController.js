b455ad4579f44027a0c5d31b0eb8b49f5dd78464
rishav <rishavn@jcsoftwaresolution.com>
2025-11-20 12:17:40 +0530

    dashboard affiliate showing correct

--- api/controllers/EmailTemplateController.js
+++ api/controllers/EmailTemplateController.js
@@ -6,7 +6,7 @@
  */
 const constants = require("../../config/constants").constants;
 const db = sails.getDatastore().manager;
-const ObjectId = require('mongodb').ObjectId;
+const ObjectId = require("mongodb").ObjectId;
 const Services = require("../services/index");
 const Joi = require("joi");
 const Validations = require("../Validations");
@@ -17,10 +17,8 @@ const Emails = require("../Emails/index");
 //check emailtemplate
 exports.create = async (req, res) => {
   try {
-    let validation_result = await Validations.EmailTemplateValidation.addEmailTemplate(
-      req,
-      res
-    );
+    let validation_result =
+      await Validations.EmailTemplateValidation.addEmailTemplate(req, res);
 
     if (validation_result && !validation_result.success) {
       throw validation_result.message;
@@ -28,19 +26,24 @@ exports.create = async (req, res) => {
 
     let data = req.body;
 
-    let isEmailTemplateExists = await EmailTemplate.findOne({ templateName: data.templateName, addedBy: req.identity.id });
+    let isEmailTemplateExists = await EmailTemplate.findOne({
+      templateName: data.templateName,
+      addedBy: req.identity.id,
+    });
 
     if (isEmailTemplateExists) {
       throw constants.EMAILTEMPLATE.ALREADY_EXISTS;
     }
 
     if (data.campaign_id) {
-      let get_campaign = await Campaign.findOne({ id: data.campaign_id, isDeleted: false });
+      let get_campaign = await Campaign.findOne({
+        id: data.campaign_id,
+        isDeleted: false,
+      });
       if (!get_campaign) {
         throw constants.EMAILTEMPLATE.INVALID_CAMPAIGN_ID;
       }
     }
-
 
     query1 = {
       addedBy: req.identity.id,
@@ -64,7 +67,7 @@ exports.create = async (req, res) => {
       status: "accepted",
       isDeleted: false,
       isActive: true,
-      campaign_id: data.campaign_id
+      campaign_id: data.campaign_id,
     });
     // console.log("brandaffiliateassocation",BrandAffiliateAssociations)
     let listOfAcceptedInvites = BrandAffiliateAssociations;
@@ -143,10 +146,14 @@ exports.create = async (req, res) => {
         });
       }
     }
-    return response.success(newTemplate, constants.EMAILTEMPLATE.CREATED, req, res);
-
+    return response.success(
+      newTemplate,
+      constants.EMAILTEMPLATE.CREATED,
+      req,
+      res
+    );
   } catch (err) {
-    console.log(err, 'err')
+    console.log(err, "err");
     return response.failed(null, `${err}`, req, res);
   }
 };
@@ -155,21 +162,29 @@ exports.read = async (req, res) => {
     if (!req.query.id) {
       throw constants.COMMON.ID_REQUIRED;
     }
-    let templates = await EmailTemplate.findOne({ id: req.query.id, isDeleted: false }).populate("campaign_id").populate("addedBy");
+    let templates = await EmailTemplate.findOne({
+      id: req.query.id,
+      isDeleted: false,
+    })
+      .populate("campaign_id")
+      .populate("addedBy");
     if (!templates) {
       throw constants.EMAILTEMPLATE.TEMPLATE_NOT_FOUND;
     }
-    return response.success(templates, constants.EMAILTEMPLATE.FETCHED, req, res);
+    return response.success(
+      templates,
+      constants.EMAILTEMPLATE.FETCHED,
+      req,
+      res
+    );
   } catch (err) {
     return res.serverError(err);
   }
 };
 exports.update = async (req, res) => {
   try {
-    let validation_result = await Validations.EmailTemplateValidation.editEmailTemplate(
-      req,
-      res
-    );
+    let validation_result =
+      await Validations.EmailTemplateValidation.editEmailTemplate(req, res);
 
     if (validation_result && !validation_result.success) {
       throw validation_result.message;
@@ -192,8 +207,14 @@ exports.update = async (req, res) => {
 
     data.updatedBy = req.identity.id;
 
-    let templates = await EmailTemplate.updateOne({ id: id }, { isDeleted: true });
-    await EmailTemplateAffiliate.update({ email_template_id: id }, { isDeleted: true });
+    let templates = await EmailTemplate.updateOne(
+      { id: id },
+      { isDeleted: true }
+    );
+    await EmailTemplateAffiliate.update(
+      { email_template_id: id },
+      { isDeleted: true }
+    );
     // return response.success(templates, constants.EMAILTEMPLATE.DELETED, req, res);
     let query1 = {
       addedBy: req.identity.id,
@@ -230,7 +251,10 @@ exports.update = async (req, res) => {
     req.body.addedBy = req.identity.id;
     req.body.updatedBy = req.identity.id;
     if (req.body.campaign_id) {
-      let get_campaign = await Campaign.findOne({ id: req.body.campaign_id, isDeleted: false });
+      let get_campaign = await Campaign.findOne({
+        id: req.body.campaign_id,
+        isDeleted: false,
+      });
       if (!get_campaign) {
         throw constants.EMAILTEMPLATE.INVALID_CAMPAIGN_ID;
       }
@@ -256,17 +280,22 @@ exports.update = async (req, res) => {
       if (emailSentCheck.emailSent == true) {
         await Emails.EmailTemplate.sendEmailTemplate(emailPayload);
       } else {
-        console.log("emailSent setting is false in emailTemplate")
+        console.log("emailSent setting is false in emailTemplate");
       }
 
       await EmailTemplateAffiliate.create({
         affiliate_id: affiliate.id,
         email_template_id: newTemplate.id,
         addedBy: req.identity.id,
-        updatedBy: req.identity.id
+        updatedBy: req.identity.id,
       });
     }
-    return response.success(newTemplate, constants.EMAILTEMPLATE.UPDATED, req, res);
+    return response.success(
+      newTemplate,
+      constants.EMAILTEMPLATE.UPDATED,
+      req,
+      res
+    );
   } catch (err) {
     return response.failed(null, `${err}`, req, res);
   }
@@ -276,11 +305,17 @@ exports.delete = async (req, res) => {
     if (!req.query.id) {
       throw constants.COMMON.ID_REQUIRED;
     }
-    let templates = await EmailTemplate.findOne({ id: req.query.id, isDeleted: false });
+    let templates = await EmailTemplate.findOne({
+      id: req.query.id,
+      isDeleted: false,
+    });
     if (!templates) {
       throw constants.EMAILTEMPLATE.TEMPLATE_NOT_FOUND;
     }
-    await EmailTemplate.updateOne({ id: req.query.id, isDeleted: false }, { isDeleted: true, updatedBy: req.identity.id });
+    await EmailTemplate.updateOne(
+      { id: req.query.id, isDeleted: false },
+      { isDeleted: true, updatedBy: req.identity.id }
+    );
     return response.success(null, constants.EMAILTEMPLATE.DELETED, req, res);
   } catch (err) {
     console.log(err);
@@ -291,13 +326,21 @@ exports.delete = async (req, res) => {
 exports.getAll = async (req, res) => {
   try {
     let query = {};
-    let count = req.param('count') || 10;
-    let page = req.param('page') || 1;
+    let count = req.param("count") || 10;
+    let page = req.param("page") || 1;
     let skipNo = (Number(page) - 1) * Number(count);
-    let { search, sortBy, status, isDeleted, format, addedBy, startDate, endDate } = req.query;
+    let {
+      search,
+      sortBy,
+      status,
+      isDeleted,
+      format,
+      addedBy,
+      startDate,
+      endDate,
+    } = req.query;
 
     if (startDate && endDate) {
-
       const start = new Date(startDate);
       start.setUTCHours(0, 0, 0, 0);
 
@@ -308,20 +351,21 @@ exports.getAll = async (req, res) => {
     }
 
     if (search) {
-      search = await Services.Utils.remove_special_char_exept_underscores(search);
+      search = await Services.Utils.remove_special_char_exept_underscores(
+        search
+      );
       query.$or = [
-        { templateName: { $regex: search, '$options': 'i' } },
-        { emailName: { $regex: search, '$options': 'i' } }
+        { templateName: { $regex: search, $options: "i" } },
+        { emailName: { $regex: search, $options: "i" } },
       ];
     }
 
     if (isDeleted) {
-      query.isDeleted = isDeleted === 'true';
+      query.isDeleted = isDeleted === "true";
     } else {
       query.isDeleted = false;
     }
 
-   
     let sortquery = {};
     if (sortBy && typeof sortBy === "string") {
       const [rawField, rawOrder] = sortBy.trim().split(/\s+/);
@@ -347,7 +391,7 @@ exports.getAll = async (req, res) => {
 
     let projection = {
       $project: {
-        templateName: {$toLower: "$templateName"},
+        templateName: { $toLower: "$templateName" },
         emailName: "$emailName",
         purpose: "$purpose",
         audience: "$audience",
@@ -449,26 +493,21 @@ exports.getAll = async (req, res) => {
       .aggregate(pipeline)
       .toArray();
 
-
     let resData = {
       total_count: totalresult ? totalresult.length : 0,
-      data: result ? result : []
+      data: result ? result : [],
     };
 
-    if (!req.param('page') && !req.param('count')) {
+    if (!req.param("page") && !req.param("count")) {
       resData.data = totalresult ? totalresult : [];
     }
 
     return response.success(resData, constants.EMAILTEMPLATE.FETCHED, req, res);
-
   } catch (error) {
-    console.log(error, "error in emailTemplate controller")
+    console.log(error, "error in emailTemplate controller");
     return response.failed(null, `${error}`, req, res);
   }
-}
-
-
-
+};
 
 // exports.getUserEmailTemplate = async (req, res) => {
 //   try {
@@ -569,7 +608,6 @@ exports.getAll = async (req, res) => {
 
 //     let totalresult = await db.collection('emailtemplateaffiliate').aggregate(pipeline).toArray();
 
-
 //     pipeline.push({
 //       $skip: Number(skipNo)
 //     });
@@ -578,7 +616,6 @@ exports.getAll = async (req, res) => {
 //     });
 
 //     let result = await db.collection('emailtemplateaffiliate').aggregate(pipeline).toArray();
-
 
 //     let resData = {
 //       total_count: totalresult ? totalresult.length : 0,
@@ -595,7 +632,6 @@ exports.getAll = async (req, res) => {
 //     return response.failed(null, `${error}`, req, res);
 //   }
 // }
-
 
 // alpha
 
@@ -617,9 +653,9 @@ exports.getUserEmailTemplate = async (req, res) => {
       endDate,
     } = req.query;
 
-
     if (search) {
-      const cleanSearch = await Services.Utils.remove_special_char_exept_underscores(search);
+      const cleanSearch =
+        await Services.Utils.remove_special_char_exept_underscores(search);
       query.$or = [
         { templateName: { $regex: cleanSearch, $options: "i" } },
         { emailName: { $regex: cleanSearch, $options: "i" } },
@@ -641,7 +677,6 @@ exports.getUserEmailTemplate = async (req, res) => {
     if (affiliate_id) query.affiliate_id = new ObjectId(affiliate_id);
     query.isDeleted = isDeleted === "true";
 
-
     let sortquery = {};
     if (sortBy && typeof sortBy === "string") {
       const [rawField, rawOrder] = sortBy.trim().split(/\s+/);
@@ -651,7 +686,6 @@ exports.getUserEmailTemplate = async (req, res) => {
     } else {
       sortquery = { updatedAt: -1 };
     }
-    
 
     const pipeline = [
       {
@@ -662,7 +696,12 @@ exports.getUserEmailTemplate = async (req, res) => {
           as: "emailtemplate_details",
         },
       },
-      { $unwind: { path: "$emailtemplate_details", preserveNullAndEmptyArrays: true } },
+      {
+        $unwind: {
+          path: "$emailtemplate_details",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
 
       {
         $lookup: {
@@ -686,7 +725,7 @@ exports.getUserEmailTemplate = async (req, res) => {
                     { $eq: ["$affiliate_id", "$$affiliateId"] },
                     { $eq: ["$isDeleted", false] },
                     { $eq: ["$status", "accepted"] },
-                    { $ne: ["$campaign_id", null] }
+                    { $ne: ["$campaign_id", null] },
                   ],
                 },
               },
@@ -707,14 +746,23 @@ exports.getUserEmailTemplate = async (req, res) => {
                     { $gt: [{ $size: "$affiliate_campaign_links" }, 0] },
                     {
                       $eq: [
-                        { $type: { $arrayElemAt: ["$affiliate_campaign_links.campaign_id", 0] } },
+                        {
+                          $type: {
+                            $arrayElemAt: [
+                              "$affiliate_campaign_links.campaign_id",
+                              0,
+                            ],
+                          },
+                        },
                         "string",
                       ],
                     },
                   ],
                 },
                 then: {
-                  $toObjectId: { $arrayElemAt: ["$affiliate_campaign_links.campaign_id", 0] },
+                  $toObjectId: {
+                    $arrayElemAt: ["$affiliate_campaign_links.campaign_id", 0],
+                  },
                 },
                 else: "$$REMOVE",
               },
@@ -745,7 +793,9 @@ exports.getUserEmailTemplate = async (req, res) => {
           campaign_id: {
             $cond: {
               if: { $gt: [{ $size: "$affiliate_campaign_links" }, 0] },
-              then: { $arrayElemAt: ["$affiliate_campaign_links.campaign_id", 0] },
+              then: {
+                $arrayElemAt: ["$affiliate_campaign_links.campaign_id", 0],
+              },
               else: null,
             },
           },
@@ -763,7 +813,7 @@ exports.getUserEmailTemplate = async (req, res) => {
               else: false,
             },
           },
-          templateName: {$toLower : "$emailtemplate_details.templateName"},
+          templateName: { $toLower: "$emailtemplate_details.templateName" },
         },
       },
 
@@ -799,14 +849,6 @@ exports.getUserEmailTemplate = async (req, res) => {
     return response.failed(null, `${error}`, req, res);
   }
 };
-
-
-
-
-
-
-
-
 
 // exports.getUserEmailTemplate = async (req, res) => {
 //   console.log("alpha")
@@ -874,7 +916,7 @@ exports.getUserEmailTemplate = async (req, res) => {
 //         $lookup: {
 //           from: "campaigns",
 //           localField: "affiliate_id",
-//           foreignField: "affiliate_id", 
+//           foreignField: "affiliate_id",
 //           as: "campaign_details"
 //         }
 //       },
@@ -888,7 +930,7 @@ exports.getUserEmailTemplate = async (req, res) => {
 //         $project: {
 //           emailtemplate_details: "$emailtemplate_details",
 //           brand_details: "$brand_details",
-//           campaign_details: "$campaign_details", 
+//           campaign_details: "$campaign_details",
 //           affiliate_id: "$affiliate_id",
 //           isDeleted: "$isDeleted",
 //           textJSONContent: "$textJSONContent",
@@ -961,7 +1003,11 @@ exports.affiliateCount = async (req, res) => {
     if (brandId) {
       // Campaigns of this brand
       get_my_total_campaigns = await Campaign.count(
-        withDateFilter({ isDeleted: false, brand_id: brandId, isArchive: false })
+        withDateFilter({
+          isDeleted: false,
+          brand_id: brandId,
+          isArchive: false,
+        })
       );
 
       // Accepted affiliates for this brand
@@ -975,6 +1021,15 @@ exports.affiliateCount = async (req, res) => {
 
       const uniqueAffiliates = _.uniq(records.map((r) => r.affiliate_id));
 
+      const validUsers = await Users.find({
+        id: uniqueAffiliates,
+        isDeleted: false,
+      }).select(["id"]);
+
+
+      // Only keep affiliates that exist and are not deleted
+      const validAffiliateIds = validUsers.map((u) => u.id);
+
       // Pending affiliates (brand needs to accept/decline)
       // const pendingRecords = await BrandAffiliateAssociation.find(
       //   withDateFilter({
@@ -983,26 +1038,24 @@ exports.affiliateCount = async (req, res) => {
       //     brand_id: brandId,
       //   })
       // );
-      
 
-      //this is old code 
+      //this is old code
       // affiliates_pending_count = pendingRecords.length;
       // console.log("affiliates_pending_coun",affiliates_pending_count)
 
       //  uniqueAffiliatesPending = _.uniq(pendingRecords.map((r) => r.affiliate_id));
       //  affiliates_pending_count = uniqueAffiliatesPending.length
 
-        const get_total_pending_campaign =  await CampaignRequestByAffiliate.find(
-      withDateFilter({
-        status:"pending",
-        isDeleted: false,
-        brand_id : brandId
-       })
-    );
+      const get_total_pending_campaign = await CampaignRequestByAffiliate.find(
+        withDateFilter({
+          status: "pending",
+          isDeleted: false,
+          brand_id: brandId,
+        })
+      );
 
-       affiliates_pending_count = get_total_pending_campaign.length
+      affiliates_pending_count = get_total_pending_campaign.length;
 
-      // console.log("affiliates_pending_coun1",affiliates_pending_count)
 
       // Find affiliates who interacted (cookies + links)
       const cookies = await Cookies.find(withDateFilter({ brand_id: brandId }));
@@ -1023,20 +1076,20 @@ exports.affiliateCount = async (req, res) => {
       const activeAffiliates = await Users.find({
         id: affiliateIds,
         status: "active",
+        isDeleted:false,
       }).select(["id"]);
 
       const activeAffiliateIds = activeAffiliates.map((a) => a.id);
 
+
       // Joined affiliates = accepted affiliates excluding actives
-      const joinedAffiliates = uniqueAffiliates.filter(
+      const joinedAffiliates = validAffiliateIds.filter(
         (id) => !activeAffiliateIds.includes(id)
-      ); 
-      
+      );
 
       associated_affiliates_count = joinedAffiliates.length;
       affiliates_active_count = activeAffiliateIds.length;
     }
-
     // Final response
     return res.status(200).json({
       success: true,
@@ -1141,4 +1194,3 @@ exports.affiliateCount = async (req, res) => {
 //     return response.failed(null, `${error}`, req, res);
 //   }
 // };
-
