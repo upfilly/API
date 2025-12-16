3e986c02eb5da3b6b676cd88ffd2cee254933e92
shiv <shiv@gmail.com>
2025-12-16 19:09:48 +0530

    fix monthly pay

--- api/controllers/SubscriptionPlansController.js
+++ api/controllers/SubscriptionPlansController.js
@@ -1778,21 +1778,22 @@ exports.webhook = async (request, response) => {
                     if (event_object.metadata.commission === "paid") {
                         try {
                             console.log("in event_object.metadata.commission")
-                            if (event_object.metadata?.transaction_ids && Array.isArray(event_object.metadata?.transaction_ids)) {
+
+                            if (event_object.metadata?.transaction_ids) {
                                 let transaction_ids = event_object.metadata?.transaction_ids;
+                                transaction_ids = transaction_ids.split(",")
                                 let invoiceId = event_object.invoice;
                                 // Get invoice details from invoiceId
                                 const invoice = await stripe.invoices.retrieve(invoiceId);
                                 console.log("Invoice = ", invoice);
                                 // Iterate through array of ids and update the invoice in transactions collection
                                 for (let transaction_id of transaction_ids) {
-                                    const get_transaction = await Transactions.findOne({id:transaction_id})
-                                    if(!get_transaction.affiliateLinkId){
+                                    const get_transaction = await Transactions.findOne({ id: transaction_id })
+                                    if (!get_transaction.affiliateLinkId) {
                                         continue
                                     }
-
-                                    await Transactions.updateOne({_id: transaction_id},{invoice_url: invoice?.url})
-                                    await AffiliateLink.updateOne({ id: get_transaction.affiliateLinkId }, { commission_paid: "paid", invoice: invoice?.hosted_invoice_url });
+                                    await Transactions.updateOne({ id: transaction_id }, { invoice_url: invoice?.hosted_invoice_url, transaction_status: event_object.payment_status })
+                                    await AffiliateLink.updateOne({ id: get_transaction.affiliateLinkId }, { commission_paid: "paid" });
                                 }
                             } else {
                                 // update user
