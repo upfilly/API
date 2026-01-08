1120f95ef3151b8ac742e3d7a4b18468e8c79c4e
rishav <rishavn@jcsoftwaresolution.com>
2026-01-08 17:09:52 +0530

    changes

--- config/routes.js
+++ config/routes.js
@@ -553,6 +553,8 @@ module.exports.routes = {
   "DELETE /affiliatelink": "AffiliateLinkController.destroy",
   "get /affiliatelink/report": "AffiliateLinkController.report",
   "put /update/commission/status": "AffiliateLinkController.updateCommission",
+  "GET /affiliatelink/all/admin": "AffiliateLinkController.find_2",
+
 
   /**
    * @FirstPromoter
